62348b625f47cb726005473548b0206f2b715bd4
xevrxst <a55817@alunos.ipb.pt>
2026-02-26 19:00:39 +0000

    initial commit

---
+++ src/exercicio2/Contador.java
@@ -0,0 +1,26 @@
+package exercicio2;
+
+public class Contador {
+    private int valor;
+    public Contador(int v){
+        valor=v;
+    }
+    public Contador() {
+        valor=0;
+    }
+    public void incrementar() {
+        valor++;
+    }
+    public void decrementar() {
+        valor--;
+    }
+    public void reset() {
+        valor=0;
+    }
+    public int get() {
+        return valor;
+    }
+    public void verificar() {
+        
+    }
+}
